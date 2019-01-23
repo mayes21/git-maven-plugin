5790b50e7ca48124b96ad314dc29e7e0b2b5c236
Amayas Abboute <amayas.abboute@live.fr>
2019-01-23 10:03:50 +0100

    check if the branch name match the regexp

--- src/main/java/com/github/amabb/GitMojo.java
+++ src/main/java/com/github/amabb/GitMojo.java
@@ -13,26 +13,70 @@ import org.eclipse.jgit.api.Git;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * MOJO to get git branch name of repository, check if match given regexp and build the suffix of the fat JAR
+ * > Default maven lifecyce phase : VALIDATE
+ */
 @Mojo(name = "git-info", defaultPhase = LifecyclePhase.VALIDATE)
 public class GitMojo extends AbstractMojo {
+
+    /**
+     * suffix for the generated fat JAR
+     */
+    @Parameter(name = "fatJarSuffix")
+    private String fatJarSuffix;
+
+    /**
+     * the regular expression that the branch name have to match
+     */
+    @Parameter(name = "regex", required = true)
+    private String regex;
+
+    /**
+     * the repository that contains .git folder
+     */
+    @Parameter(name = "basedir", required = true)
+    private String basedir;
+
+
+    @Parameter(readonly = true, defaultValue = "${project}")
+    private MavenProject project;
 
     private String branchName;
 
     private Git git;
 
-    @Parameter(readonly = true, defaultValue = "${project}")
-    private MavenProject project;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
         try {
-            git = Git.open(new File(""));
+            git = Git.open(new File(basedir));
             branchName = git.getRepository().getBranch();
+            checkBranchName(branchName, regex);
         } catch (IOException e) {
             getLog().error(e.getMessage());
         } finally {
             git.close();
         }
 
-        project.getProperties().put("branch.name", branchName);
+        project.getProperties().put(Constant.JAR_SUFFIX, checkBranchName(branchName, regex));
+    }
+
+
+    /**
+     * check if the branch name does not match the regexp
+     */
+    private String checkBranchName(String branchName, String regex) {
+        if (branchName.matches(regex)) {
+            return branchName.concat(Constant.SEPARATOR).concat(fatJarSuffix);
+        } else {
+            return Constant.SEPARATOR.concat(fatJarSuffix);
+        }
+    }
+
+
+    private class Constant {
+        Constant(){}
+        private static final String JAR_SUFFIX = "jar.suffix";
+        private static final String SEPARATOR = "-";
     }
 }
